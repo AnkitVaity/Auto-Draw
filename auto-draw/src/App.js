5c2205ad79868019e05a343615ef6d3e2b254216
Ankit Vaity <30876031+AnkitVaity@users.noreply.github.com>
2022-01-20 11:58:21 -0500

    Adding React Template

---
+++ auto-draw/src/App.js
@@ -0,0 +1,83 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Tooltip from '@mui/material/Tooltip';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Main_Container">
+
+        <div className="colors_div">
+        <Tooltip title="White">
+          <div className="c1"></div>
+        </Tooltip>
+        <Tooltip title="Lemon Yellow">
+          <div className="c2"></div>
+        </Tooltip>
+        <Tooltip title="Yellow">
+          <div className="c3"></div>
+        </Tooltip>
+          <div className="c4"></div>
+        <Tooltip title="Scarlet Red">
+          <div className="c5"></div>
+        </Tooltip>
+        <Tooltip title="Apricot Color">
+          <div className="c6"></div>
+        </Tooltip>
+        <Tooltip title="Deep Pink">
+          <div className="c7"></div>
+        </Tooltip>
+        <Tooltip title="Red">
+          <div className="c8"></div>
+        </Tooltip>
+        <Tooltip title="Maroon">
+          <div className="c9"></div>
+        </Tooltip>
+        <Tooltip title="Purple">
+          <div className="c10"></div>
+        </Tooltip>
+        <Tooltip title="Blue">
+          <div className="c11"></div>
+        </Tooltip>
+        <Tooltip title="Cyan">
+          <div className="c12"></div>
+        </Tooltip>
+          <div className="c13"></div>
+          <div className="c14"></div>
+        <Tooltip title="Turquoise">
+          <div className="c15"></div>
+        </Tooltip>
+        <Tooltip title="Green">
+          <div className="c16"></div>
+        </Tooltip>
+          <div className="c17"></div>
+        <Tooltip title="Lime Green">
+          <div className="c18"></div>
+        </Tooltip>
+        <Tooltip title="Aquamarine">
+          <div className="c19"></div>
+        </Tooltip>
+        <Tooltip title="Orange">
+          <div className="c20"></div>
+        </Tooltip>
+        <Tooltip title="Brown">
+          <div className="c21"></div>
+        </Tooltip>
+        <Tooltip title="Dark Brown">
+          <div className="c22"></div>
+        </Tooltip>
+        <Tooltip title="Gray">
+          <div className="c23"></div>
+        </Tooltip>
+        <Tooltip title="Black">
+          <div className="c24"></div>
+        </Tooltip>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
